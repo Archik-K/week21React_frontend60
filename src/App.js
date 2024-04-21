5dfdb667f7c2614b721c62f62290f247ae0795cf
Archik-K <ar.kryatova@yandex.ru>
2024-04-21 16:06:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Chart } from "react-google-charts";
+
+export const data = [
+	["Country", "Popularity"],
+	["Germany", 200],
+	["United States", 300],
+	["Brazil", 400],
+	["Canada", 500],
+	["France", 600],
+	["RU", 700],
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Географическая диаграмма</h1>
+			<Chart
+				chartEvents={[
+					{
+						eventName: "select",
+						callback: ({ chartWrapper }) => {
+							const chart = chartWrapper.getChart();
+							const selection = chart.getSelection();
+							if (selection.length === 0) return;
+							const region = data[selection[0].row + 1];
+							console.log("Selected : " + region);
+						},
+					},
+				]}
+				chartType="GeoChart"
+				width="100%"
+				height="400px"
+				data={data}
+			/>
+		</div>
+	);
 }
 
 export default App;
